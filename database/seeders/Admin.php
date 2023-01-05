a67b86c89eec85399a578c597e5aca1596750836
Megyeri Márk Máté <megyko1234@gmail.com>
2023-01-05 15:37:53 +0100

    Admin bejelentkezés sikeres!

--- database/seeders/Admin.php
+++ database/seeders/Admin.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
 
 class Admin extends Seeder
 {
@@ -14,6 +17,14 @@ class Admin extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            "name" => 'Admin',
+            "email" => "a@lan.hu",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'is_admin' => 1
+
+        ]);
     }
 }
